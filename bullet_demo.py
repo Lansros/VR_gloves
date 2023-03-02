b1066151fdc882aef26d75191bdf666c5aaced07
86178 <1453770575@qq.com>
2023-03-02 13:05:25 +0800

    Initial commit

---
+++ bullet_demo.py
@@ -0,0 +1,52 @@
+import pybullet as p
+import time
+import pybullet_data
+
+# 连接物理引擎
+physicsCilent = p.connect(p.GUI)
+
+# 渲染逻辑
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+# 添加资源路径
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# 设置环境重力加速度
+p.setGravity(0, 0, -10)
+
+# 加载URDF模型，此处是加载蓝白相间的陆地
+planeId = p.loadURDF("plane.urdf")
+
+# 加载机器人，并设置加载的机器人的位姿
+startPos1 = [0, 0, 1]
+startPos2 = [0, 0, 5]
+startPos3 = [0, 0, 10]
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+boxId = p.loadURDF("r2d2.urdf", startPos1, startOrientation)
+cube1 = p.loadURDF("sphere2.urdf", startPos2, startOrientation)
+cube2 = p.loadURDF("sphere2red.urdf", startPos3, startOrientation)
+cube3 = p.loadURDF("soccerball.urdf")
+# 按照位置和朝向重置机器人的位姿，由于我们之前已经初始化了机器人，所以此处加不加这句话没什么影响
+p.resetBasePositionAndOrientation(boxId, startPos1, startOrientation)
+
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
+# 开始一千次迭代，也就是一千次交互，每次交互后停顿1/240
+
+
+# for i in range(1000):
+#     p.stepSimulation()
+#     time.sleep(1 / 240)
+p.setRealTimeSimulation(1)
+while True:
+    pass
+
+# 获取位置与方向四元数
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print("-" * 20)
+print(f"机器人的位置坐标为:{cubePos}\n机器人的朝向四元数为:{cubeOrn}")
+print("-" * 20)
+
+# 断开连接
+p.disconnect()
